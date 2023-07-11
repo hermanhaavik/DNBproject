e52ef9e5c3ba800ab860bcdfef3bde2cc55e73e1
Herman.Fiksdahl.Haavik <herman.fiksdahl.haavik@dnb.no>
2023-07-11 12:39:00 +0000

    hauken kommer... Og han er sur

--- app/frontend/src/components/Answer/AnswerIcon.tsx
+++ app/frontend/src/components/Answer/AnswerIcon.tsx
@@ -1,5 +1,7 @@
 import { Sparkle28Filled } from "@fluentui/react-icons";
+import Fish from '@dnb/eufemia/icons/fish'
 
 export const AnswerIcon = () => {
-    return <Sparkle28Filled primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Answer logo" />;
+    return <Fish/>
+    /*return <Sparkle28Filled primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Answer logo" />;*/
 };
