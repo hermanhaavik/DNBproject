195ee26592c66d40ec10347f92b182f8636697f1
alexauren <alexander.auren@dnb.no>
2023-07-20 11:14:13 +0200

    start changing agent to conversational

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -5,7 +5,8 @@ from azure.search.documents.models import QueryType
 from langchain.llms.openai import AzureOpenAI
 from langchain.callbacks.manager import CallbackManager, Callbacks
 from langchain.chains import LLMChain
-from langchain.agents import Tool, ZeroShotAgent, AgentExecutor
+from langchain.agents import Tool, AgentType, initialize_agent, ConversationalAgent
+from langchain.memory import ConversationBufferMemory
 from langchainadapters import HtmlCallbackHandler
 from text import nonewlines
 from typing import Any, Sequence
@@ -26,51 +27,64 @@ class ChatReadRetrieveReadApproach(Approach):
 
     template_prefix = \
 "You are an intelligent assistant. Your name is Floyd. Your job is helping DNB Bank ASA customers with their questions about insurance." \
+"If the question is incomplete, ask the user for more information. " \
+"If you cannot answer the question using the sources below, stop the thought process, say that you don't know, and that the user should contact customer support. " \
+"\n\nYou can access the following tools: "
+
+    memory = ConversationBufferMemory(memory_key = "chat_history")
+
+    template_suffix = """
+Begin!
+
+Previous conversation history:
+{memory}
+
+New input:
+{input}
+{agent_scratchpad}"""  
+
+    format_instructions = """To use a tool, please use the following format:
+
+    ```
+    Thought: Do I need to use a tool? Yes
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ```
+    
+    When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+    
+    ```
+    Thought: Do I need to use a tool? No
+    {ai_prefix}: [your response here]
+    """
+
+   
+#     query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base about insurance.
+#     Generate a search query based on the conversation and the new question. 
+#     Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
+#     Do not include any text inside [] or <<>> in the search query terms.
+#     If the question is not in English, translate the question to English before generating the search query.
+
+# Chat History:
+# {memory}
+
+# Question:
+# {question}
+
+# Search Query:
+# """
+    #ai_prefix = 
+
+    human_prefix = \
 "For information in table format return it as an html table. Do not return markdown format. " \
 "Each source has a name followed by colon and the actual data, quote the source name for each piece of data you use in the response. " \
 "For example, if the question is \"What color is the sky?\" and one of the information sources says \"info123: the sky is blue whenever it's not cloudy\", then answer with \"The sky is blue [info123]\" " \
 "It's important to strictly follow the format where the name of the source is in square brackets at the end of the sentence, and only up to the prefix before the colon (\":\"). " \
 "If there are multiple sources, cite each one in their own square brackets. For example, use \"[info343][ref-76]\" and not \"[info343,ref-76]\". " \
 "Never quote tool names or chat history as sources." \
-"If the question is incomplete, ask the user for more information. " \
-"If you cannot answer the question using the sources below, stop the thought process, say that you don't know, and that the user should contact customer support. " \
-"\n\nYou can access the following tools: "
-
-    template_suffix = """
-Begin!
-
-Why do I get an error message here?
-
-
-Question: {input}
-Thought: {agent_scratchpad}"""  
-
-    format_instructions = """
-Use the following format:
-
-Question: the input question you must answer.
-Thought: you should always think about what to do to find the answer. 
-Action: the action to take to find the answer, should be one of [{tool_names}].
-Action Input: the input to the action.
-Observation: the result of the action.
-... (this Thought/Action/Action Input/Observation can repeat N times).
-Thought: I now know the final answer.
-Final Answer: Based on my observations, I now know the final answer to the original input question."""
-
-    query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base about insurance.
-    Generate a search query based on the conversation and the new question. 
-    Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
-    Do not include any text inside [] or <<>> in the search query terms.
-    If the question is not in English, translate the question to English before generating the search query.
-
-Chat History:
-{chat_history}
-
-Question:
-{question}
-
-Search Query:
-"""
+"Answer in the same language as the question was asked. "
+    
     CognitiveSearchToolDescription = "Useful for searching for public information about DNB insurance car insurance, etc."
 
     def __init__(self, search_client: SearchClient, openai_deployment: str, sourcepage_field: str, content_field: str):
@@ -124,30 +138,37 @@ Search Query:
                         callbacks=cb_manager)
         tools = [acs_tool, ask_user_tool]
 
-        prompt = ZeroShotAgent.create_prompt(
+        prompt = ConversationalAgent.create_prompt(
             tools=tools,
             prefix=overrides.get("prompt_template_prefix") or self.template_prefix,
             suffix=overrides.get("prompt_template_suffix") or self.template_suffix,
-            input_variables = ["input", "agent_scratchpad"])
+            human_prefix=self.human_prefix,
+            format_instructions=self.format_instructions,
+            input_variables=["input", "agent_scratchpad"],
+            memory=self.memory)
+        
         print(prompt)
         llm = AzureOpenAI(deployment_name=self.openai_deployment, temperature=overrides.get("temperature") or 0, openai_api_key=openai.api_key)
         chain = LLMChain(llm = llm, prompt = prompt)
-        agent_exec = AgentExecutor.from_agent_and_tools(
-            agent = ZeroShotAgent(llm_chain = chain, tools = tools),
-            tools = tools, 
-            verbose = True, 
-            callback_manager = cb_manager)
-        result = agent_exec.run(q)
+        conversational_agent = initialize_agent(
+            agent=ConversationalAgent(llm_chain = chain, tools = tools),
+            tools=tools, 
+            llm=llm,
+            verbose=True,
+            max_iterations=5,
+            memory=ConversationBufferMemory(memory_key = "chat_history"))
+        
+        result = conversational_agent(q)
                 
         # Remove references to tool names that might be confused with a citation
         result = result.replace("[CognitiveSearch]", "")
 
         return {"data_points": self.results or [], "answer": result, "thoughts": cb_handler.get_and_reset_log()}
 
-    def get_chat_history_as_text(self, history: Sequence[dict[str, str]], include_last_turn: bool=True, approx_max_tokens: int=1000) -> str:
-        history_text = ""
-        for h in reversed(history if include_last_turn else history[:-1]):
-            history_text = """<|im_start|>user""" + "\n" + h["user"] + "\n" + """<|im_end|>""" + "\n" + """<|im_start|>assistant""" + "\n" + (h.get("bot", "") + """<|im_end|>""" if h.get("bot") else "") + "\n" + history_text
-            if len(history_text) > approx_max_tokens*4:
-                break    
-        return history_text
+    # def get_chat_history_as_text(self, history: Sequence[dict[str, str]], include_last_turn: bool=True, approx_max_tokens: int=1000) -> str:
+    #     history_text = ""
+    #     for h in reversed(history if include_last_turn else history[:-1]):
+    #         history_text = """<|im_start|>user""" + "\n" + h["user"] + "\n" + """<|im_end|>""" + "\n" + """<|im_start|>assistant""" + "\n" + (h.get("bot", "") + """<|im_end|>""" if h.get("bot") else "") + "\n" + history_text
+    #         if len(history_text) > approx_max_tokens*4:
+    #             break    
+    #     return history_text
