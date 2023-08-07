9689c48f7da2835587f8465fd93f7baf39a28158
Njål Telstø <njal.brynhildarson.telsto@dnb.no>
2023-08-07 16:37:39 +0200

    Fixed supporting content tab

--- app/frontend/src/components/SupportingContent/SupportingContentParser.ts
+++ app/frontend/src/components/SupportingContent/SupportingContentParser.ts
@@ -4,6 +4,20 @@ type ParsedSupportingContentItem = {
 };
 
 export function parseSupportingContentItem(item: string): ParsedSupportingContentItem {
+    // Assumes the item has the format ###url### content
+    const match = item.match(/###(.*?)###\s*(.*)/);
+    if (match && match[1] && match[2]) {
+        return {
+            title: match[1],
+            content: match[2]
+        };
+    };
+
+    return {
+        title: "",
+        content: ""
+    };
+
     // Assumes the item starts with the file name followed by : and the content.
     // Example: "sdp_corporate.pdf: this is the content that follows".
     const parts = item.split(": ");
